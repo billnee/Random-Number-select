76f07d3a2681c02f5d6c324327255c673b98d7ad
Nguyễn Billnee <nguyencaocuong@macbook-pro-cua-nguyen.local>
2019-04-09 01:18:56 +0700

    no comment

--- Random Number select/ViewController.swift
+++ Random Number select/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Git Tutorial")
         print("Hi there, this is branch develop")
+         print("Hello, this is commit to remote")
    
     updateRandomNumber()
     
