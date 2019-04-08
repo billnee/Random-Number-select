2f5e3a934dbb28fda8664dac6d72736a69b592d8
Nguyễn Billnee <nguyencaocuong@macbook-pro-cua-nguyen.local>
2019-04-09 00:58:59 +0700

    1. Khai báo biến 2. Hàm updateRandomNumber

--- Random Number select/ViewController.swift
+++ Random Number select/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var random30 : Int = 0
+    var random2 : Int = 0
+    var numDau : Int = 0
+    var numCuoi : Int = 0
+    @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var num30: UITextField!
+    
+    @IBOutlet weak var txtDau: UITextField!
+    
+    @IBOutlet weak var txtCuoi: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Git Tutorial")
+   
+    updateRandomNumber()
+    
     }
 
-
+    @IBAction func brnRoll(_ sender: Any) {
+  updateRandomNumber()
+    }
+    func updateRandomNumber(){
+        random30 = Int(arc4random_uniform(30))+1
+        random2 = Int(arc4random_uniform(2))+1
+        num2.text = String(random2)
+        num30.text = String(random30)
+        numDau = Int(arc4random_uniform(10))
+        numCuoi = Int(arc4random_uniform(10))
+        txtDau.text = String(numDau)
+        txtCuoi.text = String(numCuoi)
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+       updateRandomNumber()
+    }
+    
 }
 
