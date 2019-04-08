7e3fdbc94c70ddb294186b54cf2afddcc8fd517e
Nguyễn Billnee <nguyencaocuong@macbook-pro-cua-nguyen.local>
2019-04-09 01:06:36 +0700

    Tạo branch develop

--- Random Number select/ViewController.swift
+++ Random Number select/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Git Tutorial")
+        print("Hi there, this is branch develop")
    
     updateRandomNumber()
     
