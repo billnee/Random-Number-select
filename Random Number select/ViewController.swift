5702470f36255b24416e5e2ed165ac7e33dff990
Nguyễn Billnee <nguyencaocuong@macbook-pro-cua-nguyen.local>
2019-04-08 22:57:23 +0700

    Initial Commit

---
+++ Random Number select/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random Number select
+//
+//  Created by Nguyễn Cao Cuong on 08/04/2019.
+//  Copyright © 2019 Nguyễn Cao Cuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
